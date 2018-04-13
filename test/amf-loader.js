01facaf36d00aa6757b0c5169c7610d0089339fe
Pawel Psztyc <jarrodek@gmail.com>
2018-04-13 14:12:44 -0700

    Initial commit

---
+++ test/amf-loader.js
@@ -0,0 +1,26 @@
+const AmfLoader = {};
+AmfLoader.load = function(index) {
+  index = index || 0;
+  const url = location.protocol + '//' + location.host +
+    location.pathname.substr(0, location.pathname.lastIndexOf('/'))
+    .replace('/test', '/demo') + '/amf-model.json';
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', (e) => {
+      let data;
+      try {
+        data = JSON.parse(e.target.response);
+      } catch (e) {
+        console.log(e.target.response);
+        reject(e);
+        return;
+      }
+      const d = data[0]['http://raml.org/vocabularies/document#declares'];
+      resolve([data, d[index]]);
+    });
+    xhr.addEventListener('error',
+      () => reject(new Error('Unable to load model file')));
+    xhr.open('GET', url);
+    xhr.send();
+  });
+};
